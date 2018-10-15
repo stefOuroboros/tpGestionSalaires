6d3fb9106e791dd5b6bccf5fda830e78e7b2b8f8
stefOuroboros <stephane.gallois89@gmail.com>
2018-10-15 13:49:15 +0200

    "tp started"

---
+++ src/main/java/POO/tpGestionSalaires/Personne.java
@@ -0,0 +1,28 @@
+package POO.tpGestionSalaires;
+
+public class Personne {
+
+	private String name;
+	private String surname;
+	private int healthID;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	/**
+	 * @param name
+	 * @param surname
+	 * @param healthID
+	 */
+	public Personne(String name, String surname, int healthID) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.healthID = healthID;
+	}
+
+}
